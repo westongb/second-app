ee4909710d4ea0e377dc77e3269981205cdea41b
westongb <westongb@outlook.com>
2019-06-13 22:29:22 -0600

    6-13 commit

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,59 +1,72 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import "./Quiz.css";
 
-const App = props => {
-  const [state, setState] = useState({
-    answer: 0
-  });
-};
+class Quiz extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      answer: "Choose..."
+    };
+    this.answerHandler = this.answerHandler.bind(this);
+  }
 
-const answerHandler = answer => {
-  setState({ answer: answer });
-};
+  answerHandler(event) {
+    console.log("Because");
+  }
 
-function Quiz() {
-  return (
-    <span className="quizArea">
-      <a className="quizCard">
-        <span>Trial</span>
-        <form>
-          <div className="form-row align-items-center">
-            <div className="col-auto my-1">
-              <label className="mr-sm-2 sr-only" for="inlineFormCustomSelect">
-                Preference
-              </label>
-              <select
-                className="custom-select mr-sm-2"
-                id="inlineFormCustomSelect"
-              >
-                <option selected>Choose...</option>
-                <option value="1" onChange={answerHandler.bind(null, 1)}>
-                  Very Inaccurate
-                </option>
-                <option value="2" onChange={answerHandler.bind(null, 2)}>
-                  Moderate Inaccurate
-                </option>
-                <option value="3" onChange={answerHandler.bind(null, 3)}>
-                  Nutrual
-                </option>
-                <option value="4" onChange={answerHandler.bind(null, 4)}>
-                  Moderately Accurate
-                </option>
-                <option value="5" onChange={answerHandler.bind(null, 5)}>
-                  Very Accurate
-                </option>
-              </select>
+  handleSubmit(event) {
+    alert("A name was submitted: " + this.state.value);
+    event.preventDefault();
+    console.log(this.state.answer);
+  }
+
+  render() {
+    return (
+      <span className="quizArea">
+        <a className="quizCard">
+          <span>Trial</span>
+          <form onSubmit={this.handleSubmit}>
+            <div className="form-row align-items-center">
+              <div className="col-auto my-1">
+                <label
+                  className="mr-sm-2 sr-only"
+                  htmlFor="inlineFormCustomSelect"
+                >
+                  Preference
+                </label>
+                <select
+                  className="custom-select mr-sm-2"
+                  id="inlineFormCustomSelect"
+                >
+                  <option>{this.state.answer}</option>
+                  <option value="1" onChange={this.answerHandler.bind(1)}>
+                    Very Inaccurate
+                  </option>
+                  <option value="2" onChange={this.answerHandler.bind(2)}>
+                    Moderate Inaccurate
+                  </option>
+                  <option value="3" onChange={this.answerHandler.bind(3)}>
+                    Nutrual
+                  </option>
+                  <option value="4" onChange={this.answerHandler.bind(4)}>
+                    Moderately Accurate
+                  </option>
+                  <option value="5" onChange={this.answerHandler.bind(5)}>
+                    Very Accurate
+                  </option>
+                </select>
+              </div>
+              <div className="col-auto my-1">
+                <button type="submit" className="btn btn-primary">
+                  Submit
+                </button>
+              </div>
             </div>
-            <div className="col-auto my-1">
-              <button type="submit" className="btn btn-primary">
-                Submit
-              </button>
-            </div>
-          </div>
-        </form>
-      </a>
-    </span>
-  );
+          </form>
+        </a>
+      </span>
+    );
+  }
 }
 
 export default Quiz;
